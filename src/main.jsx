c60d3731db27d49001cab598b958ccbc67364832
ianK-code <kian99564@gmail.com>
2023-07-06 01:35:08 +0300

    Adding register page

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Register } from "./pages/auth/Register.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path={""} element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+    {/* <App /> */}
+  </React.StrictMode>
+);
---
+++ src/pages/auth/Register.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Formik, Form, Field } from "formik";
+import { signupValidator } from "../../utils/validators/auth.validators";
+
+export const Register = () => (
+  <div>
+    <h1>Signup</h1>
+    <Formik
+      initialValues={{
+        firstName: "",
+        lastName: "",
+        email: "",
+      }}
+      validationSchema={signupValidator}
+      onSubmit={(values) => {
+        // same shape as initial values
+        console.log(values);
+      }}
+    >
+      {({ errors, touched }) => (
+        <Form>
+          <Field name="firstName" />
+          {errors.firstName && touched.firstName ? (
+            <div>{errors.firstName}</div>
+          ) : null}
+          <Field name="lastName" />
+          {errors.lastName && touched.lastName ? (
+            <div>{errors.lastName}</div>
+          ) : null}
+          <Field name="email" type="email" />
+          {errors.email && touched.email ? <div>{errors.email}</div> : null}
+          <button type="submit">Submit</button>
+        </Form>
+      )}
+    </Formik>
+  </div>
+);
